2aceedee561210e8ff982ee1899ee30c68eda81c
Garrett Finucane <garrettdreyfus@gmail.com>
2017-03-29 18:08:42 -0400

    matrix

--- sender/sender.ino
+++ sender/sender.ino
@@ -1,39 +1,29 @@
 #include <TimerOne.h>
+#include <MatrixMath.h>
 
 const int pingPin = 7;
 int index =0;
+
+const int len=8;
+int datastring[4] = {0,1,1,0};
+int hammingstring[8] = {0};
+const float encodingMatrix[4][8] =
+  {
+    {1,1,1,0,0,0,0,0},
+    {0,0,0,1,1,0,0,0},
+    {1,1,0,1,0,1,0,0},
+    {0,1,0,1,0,1,1,0}
+  }
+
+void dataToHamming(){
+  Matrix.Multiply((float *)datastring, (float *) encodingMatrix,1,4,8,(float *) hammingstring);
+}
 void setup() {
   Serial.begin(9600);
     Timer1.initialize(500000);         // initialize timer1, and set a 1/2 second period
   Timer1.attachInterrupt(increment);
   dataToHamming();
   
-}
-const int len=16;
-int datastring[11] = {1,0,1,1,0,1,1,0,0,0,0};
-int hammingstring[16] = {0};
-void dataToHamming(){
-  for(int i=0;i<16;i++){
-    hammingstring[i]=0;
-  }
-  hammingstring[2] = datastring[0];
-  hammingstring[4] = datastring[1];
-  hammingstring[5] = datastring[2];
-  hammingstring[6] = datastring[3];
-  hammingstring[8] = datastring[4];
-  hammingstring[9] = datastring[5];
-  hammingstring[10] = datastring[6];
-  hammingstring[11] = datastring[7];
-  hammingstring[12] = datastring[8];
-  hammingstring[13] = datastring[9];
-  hammingstring[14] = datastring[10];
-  for(int parityindex=0;parityindex<4;parityindex++){
-    for(int index=parityindex; index<=14; index+=pow(2,parityindex+1)){
-      for(int consecutive=0;consecutive<pow(2,parityindex);consecutive++){
-        hammingstring[(int)(pow(2,parityindex)-1)]=(hammingstring[(int)(pow(2,parityindex)-1)]+hammingstring[consecutive])%2;
-      }
-    }
-  }
 }
 
 boolean transmitting = false;
